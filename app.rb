c4992c8b5c58a964da7f2a2233af431e3e61e5c1
github username <github email address>
2019-03-20 09:01:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -11,4 +11,12 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
+  post '/puppy' do
+    @pup = Puppy.new(params[:name], params[:breed], params[:age])
+    @name = @pup.name
+    @breed = @pup.breed
+    @age = @pup.months_old
+    erb :display_puppy
+  end
+
 end
--- models/puppy.rb
+++ models/puppy.rb
@@ -4,7 +4,7 @@ class Puppy
   attr_accessor :name, :months_old
   attr_reader :breed
 
-  def initialize(name:, breed:, months_old:)
+  def initialize(name, breed, months_old)
     @name = name
     @breed = breed
     @months_old = months_old
